7b04eaded09b83ad3ec513b29a7eba97200eac81
Shashank R Pamadi <115693682+ShashankRPamadi@users.noreply.github.com>
2022-12-07 08:52:06 +0000

    printed  my name

---
+++ set01/problem02.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main() 
+{ 
+    int num1, num2, sum; 
+      
+    printf("Enter two numbers: ");print
+    scanf("%d %d", &num1, &num2); 
+      
+    // Calculate sum 
+    sum = num1 + num2; 
+      
+    // Print the sum 
+    printf("Sum of the numbers = %d", sum); 
+      
+    return 0; 
+}
